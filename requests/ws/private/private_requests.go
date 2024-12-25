471a2a20d41291f05e87c90a89ed74c5038d55d5
ApacheMoy <liam.li@qiyihuo.com>
2024-12-25 16:20:59 +0800

    first commit

---
+++ requests/ws/private/private_requests.go
@@ -0,0 +1,22 @@
+package private
+
+type (
+	Account struct {
+		Ccy string `json:"ccy,omitempty"`
+	}
+	Position struct {
+		Uly      string              `json:"uly,omitempty"`
+		InstID   string              `json:"instId,omitempty"`
+		InstType okex.InstrumentType `json:"instType"`
+	}
+	Order struct {
+		Uly      string              `json:"uly,omitempty"`
+		InstID   string              `json:"instId,omitempty"`
+		InstType okex.InstrumentType `json:"instType"`
+	}
+	AlgoOrder struct {
+		Uly      string              `json:"uly,omitempty"`
+		InstID   string              `json:"instId,omitempty"`
+		InstType okex.InstrumentType `json:"instType"`
+	}
+)
